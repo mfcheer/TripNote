c58cfd8200a8184d9e39af3b675d4af5f172ef7d
mfcheer <469236905@qq.com>
2026-09-11 17:41:04 +0800

    fix: align timeline connector beside time column

--- src/components/TimelineView.tsx
+++ src/components/TimelineView.tsx
@@ -1004,7 +1004,8 @@ function DaySection({
         <SortableContext items={items.map((i) => i.id)} strategy={verticalListSortingStrategy}>
           <div className="relative pl-[44px] sm:pl-[52px]">
             {/* 竖线 */}
-            <div className="absolute top-2 bottom-[60px] left-[64px] w-px bg-border sm:left-[76px]" />
+            {/* 竖线放在时间列与事项之间，避免穿过时间文字；小屏与桌面列宽不同，分别对齐。 */}
+            <div className="absolute top-2 bottom-[60px] left-[88px] w-px bg-border sm:left-[110px]" />
             <div className="flex flex-col gap-2.5">
               {items.map((a, index) => (
                 <div key={a.id} className="relative flex min-w-0 items-start gap-2 sm:gap-3" data-activity-id={a.id}>
